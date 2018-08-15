e7f55b61567d01736bc9995f4e44f2279c4c1d6e
T5750 <evangel_a@sina.com>
2018-08-15 14:20:36 +0800

    updated the status bar.

--- app/src/main/java/com/evangel/pedometer/activity/MainActivity.java
+++ app/src/main/java/com/evangel/pedometer/activity/MainActivity.java
@@ -75,7 +75,8 @@ public class MainActivity extends AppCompatActivity
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		// setContentView(R.layout.activity_main);
+		Globals.setContentViewAndStatusBar(this, R.layout.activity_main);
 		assignViews();
 		addListener();
 		initData();
--- app/src/main/java/com/evangel/pedometer/util/Globals.java
+++ app/src/main/java/com/evangel/pedometer/util/Globals.java
@@ -1,11 +1,19 @@
 package com.evangel.pedometer.util;
 
 import android.content.Context;
+import android.graphics.Color;
+import android.os.Build;
+import android.support.v4.view.ViewCompat;
+import android.support.v7.app.AppCompatActivity;
 import android.text.SpannableString;
 import android.text.SpannableStringBuilder;
 import android.text.Spanned;
 import android.text.style.RelativeSizeSpan;
 import android.text.style.SuperscriptSpan;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Toast;
 
 public class Globals {
@@ -88,5 +96,42 @@ public class Globals {
 	public static void showToast(Context context, String msg) {
 		Toast toast = Toast.makeText(context, msg, Toast.LENGTH_SHORT);
 		toast.show();
+	}
+
+	/**
+	 * 设置透明状态栏
+	 */
+	public static void setContentViewAndStatusBar(AppCompatActivity activity,
+			int resource) {
+		// 隐藏标题栏
+		// supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
+		View root = LayoutInflater.from(activity).inflate(resource, null);
+		// 或在界面的根层加入android:fitsSystemWindows=”true” 这个属性，这样就可以让内容界面从状态栏下方开始
+		ViewCompat.setFitsSystemWindows(root, true);
+		activity.setContentView(root);
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+			// Android 5.0 以上 全透明
+			Window window = activity.getWindow();
+			window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS
+					| WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+			window.getDecorView()
+					.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+							| View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+							| View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+			window.addFlags(
+					WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+			// 状态栏（以上几行代码必须，参考setStatusBarColor|setNavigationBarColor方法源码）
+			window.setStatusBarColor(Color.TRANSPARENT);
+			// 虚拟导航键
+			window.setNavigationBarColor(Color.TRANSPARENT);
+		} else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+			// Android 4.4 以上 半透明
+			Window window = activity.getWindow();
+			// 状态栏
+			window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+			// 虚拟导航键
+			window.addFlags(
+					WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+		}
 	}
 }
